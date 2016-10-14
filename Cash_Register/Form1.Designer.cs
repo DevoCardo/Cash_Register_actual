60ed3317fcf874ffb25b8f7691e9ef17e26318d0
temp <temp>
2016-10-14 13:29:23 -0400

    Add project files.

---
+++ Cash_Register/Form1.Designer.cs
@@ -0,0 +1,366 @@
+﻿namespace Cash_Register
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.smallCofLabel = new System.Windows.Forms.Label();
+            this.largeCofLabel = new System.Windows.Forms.Label();
+            this.medCofLabel = new System.Windows.Forms.Label();
+            this.smallCofText = new System.Windows.Forms.TextBox();
+            this.medCofText = new System.Windows.Forms.TextBox();
+            this.LargeCofText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.smallTeaText = new System.Windows.Forms.TextBox();
+            this.largeTeaText = new System.Windows.Forms.TextBox();
+            this.donutLabel = new System.Windows.Forms.Label();
+            this.biscuitLabel = new System.Windows.Forms.Label();
+            this.donutText = new System.Windows.Forms.TextBox();
+            this.biscuitText = new System.Windows.Forms.TextBox();
+            this.finButton = new System.Windows.Forms.Button();
+            this.subTotalLabel = new System.Windows.Forms.Label();
+            this.taxLabel = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.subTotalAmount = new System.Windows.Forms.Label();
+            this.taxAmount = new System.Windows.Forms.Label();
+            this.totalAmount = new System.Windows.Forms.Label();
+            this.givenLabel = new System.Windows.Forms.Label();
+            this.givenText = new System.Windows.Forms.TextBox();
+            this.amountBackLabel = new System.Windows.Forms.Label();
+            this.amountBackA = new System.Windows.Forms.Label();
+            this.changeButton = new System.Windows.Forms.Button();
+            this.printButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // smallCofLabel
+            // 
+            this.smallCofLabel.AutoSize = true;
+            this.smallCofLabel.Location = new System.Drawing.Point(1, 21);
+            this.smallCofLabel.Name = "smallCofLabel";
+            this.smallCofLabel.Size = new System.Drawing.Size(102, 13);
+            this.smallCofLabel.TabIndex = 0;
+            this.smallCofLabel.Text = "Small Coffee ($4.25)";
+            // 
+            // largeCofLabel
+            // 
+            this.largeCofLabel.AutoSize = true;
+            this.largeCofLabel.Location = new System.Drawing.Point(-1, 60);
+            this.largeCofLabel.Name = "largeCofLabel";
+            this.largeCofLabel.Size = new System.Drawing.Size(104, 13);
+            this.largeCofLabel.TabIndex = 1;
+            this.largeCofLabel.Text = "Large Coffee ($7.25)";
+            // 
+            // medCofLabel
+            // 
+            this.medCofLabel.AutoSize = true;
+            this.medCofLabel.Location = new System.Drawing.Point(109, 21);
+            this.medCofLabel.Name = "medCofLabel";
+            this.medCofLabel.Size = new System.Drawing.Size(114, 13);
+            this.medCofLabel.TabIndex = 2;
+            this.medCofLabel.Text = "Medium Coffee ($6.00)";
+            // 
+            // smallCofText
+            // 
+            this.smallCofText.Location = new System.Drawing.Point(4, 37);
+            this.smallCofText.Name = "smallCofText";
+            this.smallCofText.Size = new System.Drawing.Size(99, 20);
+            this.smallCofText.TabIndex = 3;
+            // 
+            // medCofText
+            // 
+            this.medCofText.Location = new System.Drawing.Point(112, 37);
+            this.medCofText.Name = "medCofText";
+            this.medCofText.Size = new System.Drawing.Size(111, 20);
+            this.medCofText.TabIndex = 4;
+            // 
+            // LargeCofText
+            // 
+            this.LargeCofText.Location = new System.Drawing.Point(2, 75);
+            this.LargeCofText.Name = "LargeCofText";
+            this.LargeCofText.Size = new System.Drawing.Size(101, 20);
+            this.LargeCofText.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Small Tea ($6.00)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(109, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Large Tea ($7.50)";
+            // 
+            // smallTeaText
+            // 
+            this.smallTeaText.Location = new System.Drawing.Point(4, 114);
+            this.smallTeaText.Name = "smallTeaText";
+            this.smallTeaText.Size = new System.Drawing.Size(99, 20);
+            this.smallTeaText.TabIndex = 8;
+            // 
+            // largeTeaText
+            // 
+            this.largeTeaText.Location = new System.Drawing.Point(112, 114);
+            this.largeTeaText.Name = "largeTeaText";
+            this.largeTeaText.Size = new System.Drawing.Size(111, 20);
+            this.largeTeaText.TabIndex = 9;
+            // 
+            // donutLabel
+            // 
+            this.donutLabel.AutoSize = true;
+            this.donutLabel.Location = new System.Drawing.Point(1, 162);
+            this.donutLabel.Name = "donutLabel";
+            this.donutLabel.Size = new System.Drawing.Size(72, 13);
+            this.donutLabel.TabIndex = 10;
+            this.donutLabel.Text = "Donut ($2.25)";
+            // 
+            // biscuitLabel
+            // 
+            this.biscuitLabel.AutoSize = true;
+            this.biscuitLabel.Location = new System.Drawing.Point(109, 162);
+            this.biscuitLabel.Name = "biscuitLabel";
+            this.biscuitLabel.Size = new System.Drawing.Size(96, 13);
+            this.biscuitLabel.TabIndex = 11;
+            this.biscuitLabel.Text = "Tea Biscuit ($2.75)";
+            // 
+            // donutText
+            // 
+            this.donutText.Location = new System.Drawing.Point(4, 178);
+            this.donutText.Name = "donutText";
+            this.donutText.Size = new System.Drawing.Size(99, 20);
+            this.donutText.TabIndex = 12;
+            // 
+            // biscuitText
+            // 
+            this.biscuitText.Location = new System.Drawing.Point(109, 178);
+            this.biscuitText.Name = "biscuitText";
+            this.biscuitText.Size = new System.Drawing.Size(114, 20);
+            this.biscuitText.TabIndex = 13;
+            // 
+            // finButton
+            // 
+            this.finButton.Location = new System.Drawing.Point(4, 204);
+            this.finButton.Name = "finButton";
+            this.finButton.Size = new System.Drawing.Size(99, 23);
+            this.finButton.TabIndex = 14;
+            this.finButton.Text = "Calculate Cost";
+            this.finButton.UseVisualStyleBackColor = true;
+            this.finButton.Click += new System.EventHandler(this.finButton_Click);
+            // 
+            // subTotalLabel
+            // 
+            this.subTotalLabel.AutoSize = true;
+            this.subTotalLabel.Location = new System.Drawing.Point(4, 234);
+            this.subTotalLabel.Name = "subTotalLabel";
+            this.subTotalLabel.Size = new System.Drawing.Size(53, 13);
+            this.subTotalLabel.TabIndex = 16;
+            this.subTotalLabel.Text = "Sub Total";
+            // 
+            // taxLabel
+            // 
+            this.taxLabel.AutoSize = true;
+            this.taxLabel.Location = new System.Drawing.Point(4, 255);
+            this.taxLabel.Name = "taxLabel";
+            this.taxLabel.Size = new System.Drawing.Size(25, 13);
+            this.taxLabel.TabIndex = 17;
+            this.taxLabel.Text = "Tax";
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.Location = new System.Drawing.Point(4, 278);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(31, 13);
+            this.totalLabel.TabIndex = 18;
+            this.totalLabel.Text = "Total";
+            // 
+            // subTotalAmount
+            // 
+            this.subTotalAmount.AutoSize = true;
+            this.subTotalAmount.Location = new System.Drawing.Point(64, 234);
+            this.subTotalAmount.Name = "subTotalAmount";
+            this.subTotalAmount.Size = new System.Drawing.Size(35, 13);
+            this.subTotalAmount.TabIndex = 19;
+            this.subTotalAmount.Text = "label6";
+            this.subTotalAmount.Visible = false;
+            // 
+            // taxAmount
+            // 
+            this.taxAmount.AutoSize = true;
+            this.taxAmount.Location = new System.Drawing.Point(64, 255);
+            this.taxAmount.Name = "taxAmount";
+            this.taxAmount.Size = new System.Drawing.Size(35, 13);
+            this.taxAmount.TabIndex = 20;
+            this.taxAmount.Text = "label7";
+            this.taxAmount.Visible = false;
+            // 
+            // totalAmount
+            // 
+            this.totalAmount.AutoSize = true;
+            this.totalAmount.Location = new System.Drawing.Point(64, 278);
+            this.totalAmount.Name = "totalAmount";
+            this.totalAmount.Size = new System.Drawing.Size(35, 13);
+            this.totalAmount.TabIndex = 21;
+            this.totalAmount.Text = "label8";
+            this.totalAmount.Visible = false;
+            // 
+            // givenLabel
+            // 
+            this.givenLabel.AutoSize = true;
+            this.givenLabel.Location = new System.Drawing.Point(4, 304);
+            this.givenLabel.Name = "givenLabel";
+            this.givenLabel.Size = new System.Drawing.Size(74, 13);
+            this.givenLabel.TabIndex = 22;
+            this.givenLabel.Text = "Amount Given";
+            // 
+            // givenText
+            // 
+            this.givenText.Location = new System.Drawing.Point(84, 304);
+            this.givenText.Name = "givenText";
+            this.givenText.Size = new System.Drawing.Size(71, 20);
+            this.givenText.TabIndex = 23;
+            // 
+            // amountBackLabel
+            // 
+            this.amountBackLabel.AutoSize = true;
+            this.amountBackLabel.Location = new System.Drawing.Point(4, 326);
+            this.amountBackLabel.Name = "amountBackLabel";
+            this.amountBackLabel.Size = new System.Drawing.Size(71, 13);
+            this.amountBackLabel.TabIndex = 24;
+            this.amountBackLabel.Text = "Amount Back";
+            // 
+            // amountBackA
+            // 
+            this.amountBackA.AutoSize = true;
+            this.amountBackA.Location = new System.Drawing.Point(81, 326);
+            this.amountBackA.Name = "amountBackA";
+            this.amountBackA.Size = new System.Drawing.Size(35, 13);
+            this.amountBackA.TabIndex = 25;
+            this.amountBackA.Text = "label3";
+            this.amountBackA.Visible = false;
+            // 
+            // changeButton
+            // 
+            this.changeButton.Location = new System.Drawing.Point(3, 342);
+            this.changeButton.Name = "changeButton";
+            this.changeButton.Size = new System.Drawing.Size(100, 24);
+            this.changeButton.TabIndex = 26;
+            this.changeButton.Text = "Calculate Change";
+            this.changeButton.UseVisualStyleBackColor = true;
+            this.changeButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // printButton
+            // 
+            this.printButton.Location = new System.Drawing.Point(141, 343);
+            this.printButton.Name = "printButton";
+            this.printButton.Size = new System.Drawing.Size(82, 23);
+            this.printButton.TabIndex = 27;
+            this.printButton.Text = "Print Receipt";
+            this.printButton.UseVisualStyleBackColor = true;
+            this.printButton.Visible = false;
+            this.printButton.Click += new System.EventHandler(this.printButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(648, 369);
+            this.Controls.Add(this.printButton);
+            this.Controls.Add(this.changeButton);
+            this.Controls.Add(this.amountBackA);
+            this.Controls.Add(this.amountBackLabel);
+            this.Controls.Add(this.givenText);
+            this.Controls.Add(this.givenLabel);
+            this.Controls.Add(this.totalAmount);
+            this.Controls.Add(this.taxAmount);
+            this.Controls.Add(this.subTotalAmount);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.taxLabel);
+            this.Controls.Add(this.subTotalLabel);
+            this.Controls.Add(this.finButton);
+            this.Controls.Add(this.biscuitText);
+            this.Controls.Add(this.donutText);
+            this.Controls.Add(this.biscuitLabel);
+            this.Controls.Add(this.donutLabel);
+            this.Controls.Add(this.largeTeaText);
+            this.Controls.Add(this.smallTeaText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LargeCofText);
+            this.Controls.Add(this.medCofText);
+            this.Controls.Add(this.smallCofText);
+            this.Controls.Add(this.medCofLabel);
+            this.Controls.Add(this.largeCofLabel);
+            this.Controls.Add(this.smallCofLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label smallCofLabel;
+        private System.Windows.Forms.Label largeCofLabel;
+        private System.Windows.Forms.Label medCofLabel;
+        private System.Windows.Forms.TextBox smallCofText;
+        private System.Windows.Forms.TextBox medCofText;
+        private System.Windows.Forms.TextBox LargeCofText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox smallTeaText;
+        private System.Windows.Forms.TextBox largeTeaText;
+        private System.Windows.Forms.Label donutLabel;
+        private System.Windows.Forms.Label biscuitLabel;
+        private System.Windows.Forms.TextBox donutText;
+        private System.Windows.Forms.TextBox biscuitText;
+        private System.Windows.Forms.Button finButton;
+        private System.Windows.Forms.Label subTotalLabel;
+        private System.Windows.Forms.Label taxLabel;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Label subTotalAmount;
+        private System.Windows.Forms.Label taxAmount;
+        private System.Windows.Forms.Label totalAmount;
+        private System.Windows.Forms.Label givenLabel;
+        private System.Windows.Forms.TextBox givenText;
+        private System.Windows.Forms.Label amountBackLabel;
+        private System.Windows.Forms.Label amountBackA;
+        private System.Windows.Forms.Button changeButton;
+        private System.Windows.Forms.Button printButton;
+    }
+}
+
---
+++ Cash_Register/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+namespace Cash_Register
+{
+    public partial class Form1 : Form
+    {
+        //Creates all of the variables to be used
+        const double COFSMALL = 4.25;
+        const double COFMED = 6.00;
+        const double COFLARGE = 7.25;
+        const double TEASMALL = 6.00;
+        const double TEALARGE = 7.50;
+        const double DONUT = 2.25;
+        const double BISCUIT = 2.75;
+        double price;
+        double smallCofA;
+        double medCofA;
+        double largeCofA;
+        double smallTeaA;
+        double largeTeaA;
+        double donutA;
+        double biscuitA;
+        double tax;
+        double finalPrice;
+        double change;
+        double changeBack;
+        double smallCof;
+        double medCof;
+        double largeCof;
+        double smallTea;
+        double largeTea;
+        double donut;
+        double biscuit;
+
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void finButton_Click(object sender, EventArgs e)
+        {
+
+            //Makes each amount variable equal to the amount text, and also converts string to double
+            smallCofA = Convert.ToDouble(smallCofText.Text);
+            medCofA = Convert.ToDouble(medCofText.Text);
+            largeCofA = Convert.ToDouble(LargeCofText.Text);
+            smallTeaA = Convert.ToDouble(smallTeaText.Text);
+            largeTeaA = Convert.ToDouble(largeTeaText.Text);
+            donutA = Convert.ToDouble(donutText.Text);
+            biscuitA = Convert.ToDouble(biscuitText.Text);
+
+            //creates final price for each product according to amount inputted
+            smallCof = smallCofA * COFSMALL;
+            medCof = medCofA * COFMED;
+            largeCof = largeCofA * COFLARGE;
+            smallTea = smallTeaA * TEASMALL;
+            largeTea = largeTeaA * TEALARGE;
+            donut = donutA * DONUT;
+            biscuit = biscuitA * BISCUIT;
+
+            //Creates variable price which will be used for reciept
+            price = (smallCof + medCof + largeCof + smallTea + largeTea + donut + biscuit);
+            tax = price * 0.13; 
+            finalPrice = price + tax;
+
+            //Shows subtotal, tax, and total prices before making receipt
+
+            Refresh();
+            Thread.Sleep(10);
+
+            subTotalAmount.Text ="$" + price.ToString("0.00");
+            subTotalAmount.Visible = true;
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            taxAmount.Text = "$" + tax.ToString("0.00");
+            taxAmount.Visible = true;
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            totalAmount.Text = "$" + finalPrice.ToString("0.00");
+            totalAmount.Visible = true;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Takes input and then calculates and displays change back
+            change = Convert.ToDouble(givenText.Text);
+            changeBack = change - finalPrice;
+            amountBackA.Text = "$" + changeBack.ToString("0.00");
+            amountBackA.Visible = true;
+            printButton.Visible = true;
+        }
+
+        private void printButton_Click(object sender, EventArgs e)
+        {
+            //Sets up graphics
+            Graphics formGraphics = this.CreateGraphics();
+            Pen blackPen = new Pen(Color.Black, 5);
+            SolidBrush blackBrush = new SolidBrush(Color.Black);
+            Graphics fg = this.CreateGraphics();
+            Font receiptFont = new Font("Arial", 8, FontStyle.Bold);
+            Font cafeFont = new Font("BrushScriptStd", 8, FontStyle.Italic);
+
+            //converts prices to string
+            
+            //Draws lines and prices
+            formGraphics.DrawLine(blackPen, 260, 0, 260, 1000);
+            formGraphics.DrawLine(blackPen, 260, 300, 1000, 300);
+            fg.DrawString("Small Coffee " + "(" + smallCofA +")" , receiptFont, blackBrush, 350, 40);
+            fg.DrawString("Medium Coffee " + "(" + medCofA + ")", receiptFont, blackBrush, 350, 80);
+            fg.DrawString("Large Coffee " + "(" + largeCofA + ")" , receiptFont, blackBrush, 350, 120);
+            fg.DrawString("Small Tea " + "(" + smallTeaA + ")", receiptFont, blackBrush, 350, 160);
+            fg.DrawString("Large Tea " + "(" + largeTeaA + ")", receiptFont, blackBrush, 350, 200);
+            fg.DrawString("Donut " + "(" + donutA + ")", receiptFont, blackBrush, 350, 240);
+            fg.DrawString("Tea Biscuit " + "(" + biscuitA + ")", receiptFont, blackBrush, 350, 280);
+            fg.DrawString(Convert.ToString(smallCof), receiptFont, blackBrush, 500, 40);
+            fg.DrawString(Convert.ToString(medCof), receiptFont, blackBrush, 500, 80);
+            fg.DrawString(Convert.ToString(largeCof), receiptFont, blackBrush, 500, 120);
+            fg.DrawString(Convert.ToString(smallTea), receiptFont, blackBrush, 500, 160);
+            fg.DrawString(Convert.ToString(largeTea), receiptFont, blackBrush, 500, 200);
+            fg.DrawString(Convert.ToString(donut), receiptFont, blackBrush, 500, 240);
+            fg.DrawString(Convert.ToString(biscuit), receiptFont, blackBrush, 500, 280);
+            fg.DrawString("MacCafe", cafeFont, blackBrush, 350, 10);
+            fg.DrawString("Order #5142, 12/5/2014", cafeFont, blackBrush, 420, 10);
+
+            //Prints price according to each item
+            /* sCReceipt.Visible = true;
+             sCReceipt.Text = "$" + smallCof;
+             mCReceipt.Visible = true;
+             mCReceipt.Text = "$" + medCof;
+             lCReceipt.Visible = true;
+             lCReceipt.Text = "$" + largeCof;
+             sTReceipt.Visible = true;
+             sTReceipt.Text = "$" + smallTea;
+             lTReceipt.Visible = true;
+             lTReceipt.Text = "$" + largeTea;
+             donutReceipt.Visible = true;
+             donutReceipt.Text = "$" + donut;
+             biscuitReceipt.Visible = true;
+             biscuitReceipt.Text = "$" + biscuit;*/
+        }
+    }
+}
